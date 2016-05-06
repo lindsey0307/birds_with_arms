9cc90c641d25070af9e118c804d8edf14c86f1e8
Fabrice Castel <fabrice.castel@hotmail.com>
2016-05-06 18:14:47 -0400

    add script n shety

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetup : MonoBehaviour {
+  [SerializeField] private GameObject birdPrefab;
+
+  void Start() {
+    var bird = GameObject.Instantiate(birdPrefab);
+    bird.transform.SetParent(this.transform);
+  }
+}
