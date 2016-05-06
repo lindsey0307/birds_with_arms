e0a4dd713645a31c19e547a6d1c240dd6840ff8e
Fabrice Castel <fabrice.castel@hotmail.com>
2016-05-06 18:58:11 -0400

    ai bitbs

--- Assets/Scripts/GameSetup.cs
+++ Assets/Scripts/GameSetup.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class GameSetup : MonoBehaviour {
   [SerializeField] private GameObject birdPrefab;
 
+  private System.Random random = new System.Random();
+
   void Start() {
     for (int i = 0; i < 10; i++) {
       var bird = GameObject.Instantiate(birdPrefab);
       bird.transform.SetParent(this.transform);
       PlayerMobility birdController = bird.GetComponent<PlayerMobility>();
-      birdController.Init(i < 5 ? i : 0);
+      birdController.Init(i < 2 ? i : 0, random);
     }
   }
 }
--- Assets/Scripts/PlayerMobility.cs
+++ Assets/Scripts/PlayerMobility.cs
@@ -1,38 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerMobility : MonoBehaviour {
   [SerializeField] private float speed;
 
 	private string horizontalCtrl;
 	private string verticalCtrl;
+  private bool isAi;
 
-  public void Init(int playerId = 0) {
-    if (playerId != 0) {
+  // stoopid ai birb movement tingz
+  private int xMove = 0;
+  private int yMove = 0;
+  private DateTime nextDirectionSwtichTime;
+  private System.Random random;
+
+  public void Init(int playerId, System.Random random) {
+    this.random = random;
+    isAi = playerId == 0;
+    if (isAi) {
+      nextDirectionSwtichTime = DateTime.Now;
+    } else {
       horizontalCtrl = "Horizontal_P" + playerId;
       verticalCtrl = "Vertical_P" + playerId;
     }
   }
 
 	protected void FixedUpdate() {
+    if (isAi) {
+      DoAiMovement();
+    } else {
+      DoPlayerMovement();
+    }
+	}
+
+  private void DoAiMovement() {
+    if (DateTime.Now >= nextDirectionSwtichTime) {
+      xMove = random.Next(-1, 2);
+      yMove = random.Next(-1, 2);
+      nextDirectionSwtichTime = DateTime.Now.AddSeconds(5);
+    }
+
+    transform.position += Vector3.right * xMove + Vector3.up * yMove;
+  }
+
+  private void DoPlayerMovement() {
     float h = Input.GetAxis(horizontalCtrl);
     float v = Input.GetAxis(verticalCtrl);
     float t = 0.01f;
 
-		if (h < -t) {
-			transform.position += Vector3.left * speed * Time.deltaTime;
-		}
+    if (h < -t) {
+      transform.position += Vector3.left * speed * Time.deltaTime;
+    }
 
-		if (h > t) {
-			transform.position += Vector3.right * speed * Time.deltaTime;
-		}
+    if (h > t) {
+      transform.position += Vector3.right * speed * Time.deltaTime;
+    }
 
-		if (v > t) {
-			transform.position += Vector3.up * speed * Time.deltaTime;
-		}
-		
+    if (v > t) {
+      transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+
     if (v < -t) {
-			transform.position += Vector3.down * speed * Time.deltaTime;
-		}
-	}
+      transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+  }
 }
