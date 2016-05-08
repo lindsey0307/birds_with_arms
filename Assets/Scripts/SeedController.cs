babe0c8532cf220b60daf948e554db30717ca38c
Fabrice Castel <fabrice.castel@hotmail.com>
2016-05-08 15:25:33 -0400

    add seedz

---
+++ Assets/Scripts/SeedController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+
+public class SeedController : MonoBehaviour {
+  [SerializeField] float maxSpawnDelaySeconds;
+  [SerializeField] GameObject seedPrefab;
+
+  private DateTime nextSpawnTime;
+  private System.Random random;
+  private GameController gameController;
+  private Rect levelBounds;
+	
+  public void Init(System.Random random, GameController gameController, Rect levelBounds) {
+    this.random = random;
+    this.levelBounds = levelBounds;
+    this.gameController = gameController;
+    this.nextSpawnTime = DateTime.Now.AddSeconds((float)random.NextDouble() * maxSpawnDelaySeconds);
+  }
+
+	void Update () {
+    if (random != null && nextSpawnTime < DateTime.Now) {
+      Debug.Log(nextSpawnTime + " / " + random);
+      this.nextSpawnTime = DateTime.Now.AddSeconds((float)random.NextDouble() * maxSpawnDelaySeconds);
+      var seed = GameObject.Instantiate(seedPrefab);
+      float x = (float)(random.NextDouble()) * this.levelBounds.width + this.levelBounds.xMin;
+      float y = (float)(random.NextDouble()) * this.levelBounds.height + this.levelBounds.yMin;
+      seed.transform.position = new Vector3(x, y, y + 1.0f);
+      gameController.AddSeed(seed);
+    }
+	}
+}
