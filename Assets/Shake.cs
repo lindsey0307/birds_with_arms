df009b0eec51249bc80e2d3cdabcf2d369bfb0d5
Fabrice Castel <fabrice.castel@hotmail.com>
2016-05-08 16:53:09 -0400

    splash screen

---
+++ Assets/Shake.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shake : MonoBehaviour {
+  private Vector3 initPos;
+  private System.Random random;
+  private float intensity = 0f;
+
+  void Start() {
+    initPos = this.transform.position;
+    random = new System.Random();
+  }
+
+	void Update () {
+    intensity += 0.002f;
+    this.transform.position = initPos + new Vector3(
+      ((float)random.NextDouble()-.5f) * intensity, 
+      ((float)random.NextDouble()-.5f) * intensity,
+      ((float)random.NextDouble()-.5f) * intensity);
+	}
+}
---
+++ Assets/Transitionner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Transitionner : MonoBehaviour {
+  private DateTime transition1;
+
+	void Start () {
+    transition1 = DateTime.Now.AddSeconds(10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (transition1 != null && DateTime.Now > transition1) {
+      SceneManager.LoadScene("2d Top Down");
+    }
+	}
+}
