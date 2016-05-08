0ccdff4a23656b52b077b80467e5a38f5c801cf9
Fabrice Castel <fabrice.castel@hotmail.com>
2016-05-08 17:02:50 -0400

    fuck your requirements

--- Assets/Transitionner.cs
+++ Assets/Transitionner.cs
@@ -3,15 +3,22 @@ using UnityEngine.SceneManagement;
 using System;
 
 public class Transitionner : MonoBehaviour {
+  public GameObject titlePanel;
   private DateTime transition1;
+  private DateTime transition2;
 
 	void Start () {
     transition1 = DateTime.Now.AddSeconds(10);
+    transition2 = DateTime.Now.AddSeconds(20);
 	}
 	
 	// Update is called once per frame
 	void Update () {
     if (transition1 != null && DateTime.Now > transition1) {
+      titlePanel.SetActive(true);
+    }
+
+    if (transition2 != null && DateTime.Now > transition2) {
       SceneManager.LoadScene("2d Top Down");
     }
 	}
