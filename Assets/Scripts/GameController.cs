ba4236a12ef4b47689218123be2856b89d0d2ddc
Fabrice Castel <fabrice.castel@hotmail.com>
2016-05-08 17:22:27 -0400

    loop working

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -10,11 +13,16 @@ public class GameController : MonoBehaviour {
   [SerializeField] private Camera camera;
   [SerializeField] private int playerCount;
   [SerializeField] private int totalBirdCount;
+  [SerializeField] private Text winnerMessage;
+
+  private int livePlayers;
 
   public List<BirdController> Birds { get; private set; }
   public List<GameObject> Seeds { get; private set; }
 
   private System.Random random = new System.Random();
+  private bool gameOver;
+  private DateTime restartTimer;
 
   public void AddSeed(GameObject seed) {
     this.Seeds.Add(seed);
@@ -36,6 +44,7 @@ public class GameController : MonoBehaviour {
     Seeds = new List<GameObject>();
 
     this.seedController.Init(random, this, bounds);
+    livePlayers = playerCount;
 
     for (int i = 1; i <= totalBirdCount; i++) {
       var bird = GameObject.Instantiate(birdPrefab);
@@ -43,6 +52,28 @@ public class GameController : MonoBehaviour {
       BirdController birdController = bird.GetComponent<BirdController>();
       birdController.Init(i, random, bounds, this, i > playerCount);
       Birds.Add(birdController);
+    }
+  }
+
+  void Update() {
+    if (gameOver && restartTimer < DateTime.Now) {
+      SceneManager.LoadScene("2d Top Down");
+    }
+  }
+
+  public void ReportPlayerDeath() {
+    livePlayers -= 1;
+    if (livePlayers == 1) {
+      int playerId = 0;
+      foreach (var bird in Birds) {
+        if (!bird.isAi && !(bird.birdState != BirdController.State.DEAD)) {
+          playerId = bird.Id;
+          break;
+        }
+      }
+      gameOver = true;
+      restartTimer = DateTime.Now.AddSeconds(10);
+      winnerMessage.text = "WINNER: Player " + playerId;
     }
   }
 }
